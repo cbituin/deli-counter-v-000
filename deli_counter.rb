3c611479736b95de01d3d953528344714dafbf92
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-09-11 22:24:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def line(customersWaiting)
+  if customersWaiting.size <= 0
+    puts 'The line is currently empty.';
+  else 
+    nameList = []
+    i = 0
+    until i === customersWaiting.size
+      currentPosition = customersWaiting.index(customersWaiting[i])
+      currentPosition+=1
+      customerName = customersWaiting[i]
+      nameList.push("#{currentPosition}. #{customerName}")
+      strList = nameList.join(" ")
+      i += 1
+    end
+    puts "The line is currently: #{strList}"
+  end
+  end
+  
+def take_a_number(customersWaiting, customerName)
+  customersWaiting.push(customerName)
+  currentPosition = customersWaiting.index(customerName)
+  currentPosition+=1
+  puts "Welcome, #{customerName}. You are number #{currentPosition} in line."
+end
